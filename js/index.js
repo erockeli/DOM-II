b7a2fd777e69baec883d85710f43550debf0681a
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2019-10-01 15:42:24 -0500

    Mouseenter & mouseleave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+const nav = document.querySelector('nav');
+
+nav.addEventListener('mouseenter',(event)=>{
+    nav.style.transition="1s";
+    nav.style.backgroundColor = "pink";
+});
+ 
+nav.addEventListener('mouseleave',(event)=>{
+    nav.style.transition="1s";
+    nav.style.backgroundColor = "white";
+});
+
