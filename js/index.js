22a46099b12d7df6508acf154c502a4cdb795814
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2019-10-01 16:13:21 -0500

    Mouseover & Mouseout

--- js/index.js
+++ js/index.js
@@ -11,3 +11,25 @@ nav.addEventListener('mouseleave',(event)=>{
     nav.style.backgroundColor = "white";
 });
 
+const anchors = document.querySelectorAll('a');
+ 
+anchors.forEach(function(item,index,array){
+    item.addEventListener("click", (event)=>{
+        // console.log('clicked', index);
+        event.preventDefault();
+    });
+});
+
+const logo = document.querySelector('.logo-heading');
+ 
+logo.addEventListener('mouseover', (event)=>{
+    logo.style.transition="1s";
+    logo.style.fontSize = "7rem";
+});
+ 
+logo.addEventListener('mouseout', (event)=>{
+    logo.style.transition="1s";
+    logo.style.fontSize = "4rem";
+});
+
+
